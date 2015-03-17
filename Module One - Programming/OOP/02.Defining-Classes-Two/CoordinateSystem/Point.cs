873cff02ca92f7a24e67c5c2714a8db59ea56917
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2015-03-17 22:08:43 +0200

    Added directory for Defining Classed Part two. Completed the first two tasks of the homework.

---
+++ Module One - Programming/OOP/02.Defining-Classes-Two/CoordinateSystem/Point.cs
@@ -0,0 +1,31 @@
+﻿namespace CoordinateSystem
+{
+    using System;
+    using System.Text;
+
+    struct Point3D
+    {
+        private static readonly Point3D pointO = new Point3D(0, 0, 0);
+        public Point3D(double x, double y, double z) : this()
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public static Point3D PointO 
+        { 
+            get { return pointO; } 
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("{");
+            result.AppendFormat("{0}, {1}, {2}", this.X, this.Y, this.Z);
+            result.Append("}");
+            return result.ToString();
+        }
+    }
+}
---
+++ Module One - Programming/OOP/02.Defining-Classes-Two/CoordinateSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoordinateSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point3D point = new Point3D(12, 2, 10);
+            Console.WriteLine(point);
+            Console.WriteLine(Point3D.PointO);
+        }
+    }
+}

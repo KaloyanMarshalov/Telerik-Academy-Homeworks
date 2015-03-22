b5f03cbe59ae0a260dc700b9c6e99304a2d4df9c
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2015-03-22 22:49:32 +0200

    Completed Defining Classes Two homework

---
+++ Module One - Programming/OOP/02.Defining-Classes-Two/Matrix/Matrix.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Text;
+
+namespace Matrix
+{
+    public class Matrix<T> where T : struct, IComparable
+    {
+        private int rows;
+        private int cols;
+        private T[,] genericMatrix;
+
+        public int Rows
+        {
+            get { return this.rows; }
+            set { this.rows = value; }
+        }
+
+        public int Cols
+        {
+            get { return this.cols; }
+            set { this.cols = value; }
+        }
+
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            this.genericMatrix = new T[this.rows, this.cols];
+        }
+
+        public Matrix(T[,] predefinedMatrix)
+        {
+            this.genericMatrix = predefinedMatrix;
+            this.rows = genericMatrix.GetLength(0);
+            this.cols = genericMatrix.GetLength(1);
+        }
+
+        public T this[int indexOfRows, int indexOfCols]
+        {
+            get
+            {
+                if (indexOfRows > this.rows - 1 || indexOfRows < 0)
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "Invalid index: {0}.", indexOfRows));
+                }
+
+                if (indexOfCols > this.cols - 1 || indexOfCols < 0)
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "Invalid index: {0}.", indexOfCols));
+                }
+                T result = this.genericMatrix[indexOfRows, indexOfCols];
+                return result;
+            }
+
+            set { this.genericMatrix[indexOfRows, indexOfCols] = value; }
+        }
+
+        public static Matrix<T> operator +(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            if (matrix1.Rows != matrix2.Rows || matrix1.Cols != matrix2.Cols)
+            {
+                throw new ArgumentException("Invalid operation!, Rowsa and cols must be equal.");
+            }
+            Matrix<T> result = new Matrix<T>(matrix1.Rows, matrix1.Cols);
+
+            for (int i = 0; i < result.Rows; i++)
+            {
+                for (int j = 0; j < result.Cols; j++)
+                {
+                    result[i, j] = (dynamic)matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix<T> operator -(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            if (matrix1.Rows != matrix2.Rows || matrix1.Cols != matrix2.Cols)
+            {
+                throw new ArgumentException("Invalid operation!, Rowsa and cols must be equal.");
+            }
+            Matrix<T> result = new Matrix<T>(matrix1.Rows, matrix1.Cols);
+
+            for (int i = 0; i < result.Rows; i++)
+            {
+                for (int j = 0; j < result.Cols; j++)
+                {
+                    result[i, j] = (dynamic)matrix1[i, j] - matrix2[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix<T> operator *(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            if (matrix1.Cols != matrix2.Rows)
+            {
+                throw new ArgumentException("Invalid operation! Rows of matrix one must equal cols of matrix 2.");
+            }
+            Matrix<T> resultMatrix = new Matrix<T>(matrix1.Rows, matrix2.Cols);
+            T result = (dynamic)0;
+            for (int i = 0; i < matrix1.Rows; i++)
+            {
+                for (int j = 0; j < matrix2.Cols; j++)
+                {
+                    for (int k = 0; k < matrix1.Cols; k++)
+                    {
+                        result += (dynamic)matrix1[i, k] * matrix2[k, j];
+                    }
+                    resultMatrix[i, j] = result;
+                    result = (dynamic)0;
+                }
+            }
+            return resultMatrix;
+        }
+
+        public override string ToString() //not required
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < this.rows; i++)
+            {
+                for (int j = 0; j < this.cols; j++)
+                {
+                    result.Append(string.Format("{0, 11}", this.genericMatrix[i, j]));
+                }
+                result.Append(Environment.NewLine);
+            }
+
+            return result.ToString();
+        }
+
+        public static bool operator true(Matrix<T> matrix)
+        {
+
+            for (int i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Cols; j++)
+                {
+                    if (matrix[i, j] == (dynamic)0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator false(Matrix<T> matrix)
+        {
+            for (int i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Cols; j++)
+                {
+                    if (matrix[i, j] == (dynamic)0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Module One - Programming/OOP/02.Defining-Classes-Two/Matrix/StartingPoint.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class StartingPoint
+    {
+        static void Main()
+        {
+            Matrix<int> testMatrix = new Matrix<int>(5, 5); 
+            testMatrix[2, 1] = 5; 
+            Console.WriteLine("Matrix[2,1] = {0}", testMatrix[2, 1]);
+
+
+            Matrix<int> matrix1 = new Matrix<int>(3, 3); //two matrices for testing +, - and *
+            Matrix<int> matrix2 = new Matrix<int>(3, 3);
+
+            for (int i = 0; i < matrix1.Rows; i++)
+            {
+                for (int j = 0; j < matrix1.Cols; j++)
+                {
+                    matrix1[i, j] = (i + 3) * (j + 1) + 5;
+                    matrix2[i, j] = (i + 1) * (j + 2);
+                }
+            }
+
+            Console.WriteLine("Matrix 1:\n{0}", matrix1);
+            Console.WriteLine("Matrix 2:\n{0}", matrix2);
+
+            Matrix<int> resultOfAddition = matrix1 + matrix2;
+            Matrix<int> resultOfSubtraction = matrix1 - matrix2;
+            Matrix<int> resultOfMultiplication = matrix1 * matrix2;
+
+            Console.WriteLine("Matrix1 + Matrix2 =\n{0}", resultOfAddition);
+            Console.WriteLine("Matrix1 - Matrix2 =\n{0}", resultOfSubtraction);
+            Console.WriteLine("Matrix1 * Matrix2 =\n{0}", resultOfMultiplication);
+
+            checkMatrix(matrix1);
+            matrix1[0, 0] = 0;
+            checkMatrix(matrix1);
+        }
+        static void checkMatrix(Matrix<int> matrix) //could not do it with Matrix<T>, would appreciate a solution :).
+        {
+            if (matrix)
+            {
+                Console.WriteLine("Does not contain zero");
+            }
+            else
+            {
+                Console.WriteLine("Contains zero");
+            }
+        }
+    }
+}

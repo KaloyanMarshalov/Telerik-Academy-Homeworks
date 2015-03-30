c563518710070d1bc29c246f1c62a119880dd091
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2015-03-30 20:49:29 +0300

    Started OOP Principales Two. Completed first task.

---
+++ Module One - Programming/OOP/05.OOP-Principales-Two/01.Shapes/Shape.cs
@@ -0,0 +1,14 @@
+﻿namespace _01.Shapes
+{
+    public abstract class Shape
+    {
+        public Shape(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public abstract double CalculateSurface();
+        public virtual double Width { get; set; }
+        public virtual double Height { get; set; }
+    }
+}
---
+++ Module One - Programming/OOP/05.OOP-Principales-Two/01.Shapes/StartingPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Shapes
+{
+    class StartingPoint
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>{
+                new Square(12.3),
+                new Rectangle(10, 3.2),
+                new Triangle(2, 3),
+                new Rectangle(7, 5.9),
+                new Rectangle(10, 3.7),
+                new Square(2),
+                new Triangle(4, 3),
+            };
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine(shape.CalculateSurface());
+            }
+        }
+    }
+}
